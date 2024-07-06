3b549d87a8635fca780d1c439f5e7add01c3f6bd
seveibar <seveibar@gmail.com>
2024-07-05 21:55:38 -0700

    add build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: process.env.BUILD_FOR_GH_PAGES ? "/jscad-fiber/" : undefined,
 })
