a3e79983e807ecd7754a998265bc884ba04448a5
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-07-10 07:40:06 +0300

    create fixtures

---
+++ examples/extrude-rotate.fixture.tsx
@@ -0,0 +1,24 @@
+import { JsCadFixture } from "../lib/components/jscad-fixture"
+import { ExtrudeRotate } from "../lib/jscad-fns"
+import { Polygon } from "../lib/jscad-fns/polygon"
+
+export default () => (
+  <JsCadFixture>
+    <ExtrudeRotate
+      angle={Math.PI}
+      segments={24}
+    >
+      <Polygon
+        points={[
+          [-2, -1],
+          [2, -1],
+          [2.5, 2],
+          [1, 1],
+          [0, 2],
+          [-1, 1],
+          [-2, 2]
+        ]}
+      />
+    </ExtrudeRotate>
+  </JsCadFixture>
+)
