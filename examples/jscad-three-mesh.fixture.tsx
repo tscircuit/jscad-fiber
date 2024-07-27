271454d041407e940f5eab87e17a22a58533d546
Richard Beattie <richard.beattie91@gmail.com>
2024-07-27 13:13:23 -0500

    Fix commitUpdate function

--- examples/jscad-three-mesh.fixture.tsx
+++ examples/jscad-three-mesh.fixture.tsx
@@ -1,20 +1,39 @@
 import { OrbitControls } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-import { Cube } from "../lib"
+import { Custom } from "../lib"
 import { JSCadThreeMesh } from "../lib/components/jscad-three-mesh"
+import { useState } from "react"
+import { cube, sphere } from "@jscad/modeling/src/primitives"
 
-export default () => (
-  <div
+export default () => {
+
+  const [solid, setSolid] = useState(cube({ size: 10 }));
+
+  const setCube = () => {
+    setSolid(cube({ size: 10 }));
+  }
+
+  const setSphere = () => {
+    setSolid(sphere({ radius: 6, segments: 32 }));
+  }
+
+  return (
+    <div
     style={{
       width: "100%",
       height: "100vh",
     }}
   >
+    <button onClick={setCube} type="button">Cube</button>
+    <button onClick={setSphere} type="button">Sphere</button>
     <Canvas>
       <JSCadThreeMesh>
-        <Cube size={10} />
+        <Custom geometry={solid} />
       </JSCadThreeMesh>
       <OrbitControls />
     </Canvas>
   </div>
-)
+  )
+
+ 
+}
