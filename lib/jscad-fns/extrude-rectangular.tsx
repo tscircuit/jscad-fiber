4dbc941c290a72426631797c2904fea37b10e860
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-08-04 11:22:06 +0300

    wrap ExtrudeRectangular

--- lib/jscad-fns/extrude-rectangular.tsx
+++ lib/jscad-fns/extrude-rectangular.tsx
@@ -1,17 +1,22 @@
+import { withColorProp } from "lib/wrappers/with-color-prop"
+import { withOffsetProp } from "lib/wrappers/with-offset-prop"
+
 export type ExtrudeRectangularProps = {
   size: number
   height: number
   children: any
 }
 
-export function ExtrudeRectangular({
+const ExtrudeRectangularBase = ({
   size,
   height,
   children,
-}: ExtrudeRectangularProps) {
+}: ExtrudeRectangularProps) => {
   return (
     <extrudeRectangular size={size} height={height}>
       {children}
     </extrudeRectangular>
   )
 }
+
+export const ExtrudeRectangular = withOffsetProp(withColorProp(ExtrudeRectangularBase))
