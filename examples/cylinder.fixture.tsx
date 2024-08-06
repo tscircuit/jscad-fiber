8583bf7477a13fcc1d7eef4e2b78ad988dab2cd4
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-08-06 05:02:14 +0000

    formatbot: Automatically format code

--- examples/cylinder.fixture.tsx
+++ examples/cylinder.fixture.tsx
@@ -3,11 +3,6 @@ import { JsCadFixture } from "../lib/components/jscad-fixture"
 
 export default () => (
   <JsCadFixture wireframe>
-    <Cylinder
-      radius={10}
-      height={20}
-      color="#F7E8AA"
-      center={[0, 0, 10]}
-    />
+    <Cylinder radius={10} height={20} color="#F7E8AA" center={[0, 0, 10]} />
   </JsCadFixture>
 )
