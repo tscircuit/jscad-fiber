0a27618c7e7bfb68f7e33d5ac2da922be107c583
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-08-08 06:40:15 +0000

    formatbot: Automatically format code

--- examples/torus.fixture.tsx
+++ examples/torus.fixture.tsx
@@ -3,11 +3,6 @@ import { JsCadFixture } from "../lib/components/jscad-fixture"
 
 export default () => (
   <JsCadFixture wireframe>
-    <Torus
-      innerRadius={1}
-      outerRadius={1.2}
-      color="red"
-      center={[0, 10, 0]}
-    />
+    <Torus innerRadius={1} outerRadius={1.2} color="red" center={[0, 10, 0]} />
   </JsCadFixture>
 )
