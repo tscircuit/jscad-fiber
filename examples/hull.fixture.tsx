a753f1d2e93a3f3d72c073e67a25eef40577a516
tscircuitbot <tscircuitbot@users.noreply.github.com>
2024-08-08 08:38:28 +0000

    formatbot: Automatically format code

--- examples/hull.fixture.tsx
+++ examples/hull.fixture.tsx
@@ -3,17 +3,9 @@ import { JsCadFixture } from "../lib/components/jscad-fixture"
 
 export default () => (
   <JsCadFixture>
-    <Hull
-      color="lightblue"
-      center={[0, -5, -10]}
-    >
-      <Cuboid
-        size={[10, 10, 10]}
-      />
-      <Ellipsoid
-        radius={[10, 10, 10]}
-        center={[0, 0, 15]}
-      />
+    <Hull color="lightblue" center={[0, -5, -10]}>
+      <Cuboid size={[10, 10, 10]} />
+      <Ellipsoid radius={[10, 10, 10]} center={[0, 0, 15]} />
     </Hull>
   </JsCadFixture>
 )
