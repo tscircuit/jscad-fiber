a35f614dd1fe483fd8ec205381c87ade116a0475
Abhijit Roy <roya51788@gmail.com>
2024-08-28 20:20:37 +0530

    fix:Refactor Rotate component & format

--- lib/jscad-fns/rotate.tsx
+++ lib/jscad-fns/rotate.tsx
@@ -1,59 +1,57 @@
-import { withColorProp } from "lib/wrappers/with-color-prop";
-import { withOffsetProp } from "lib/wrappers/with-offset-prop";
-import type { Point3 } from "./translate";
+import { withColorProp } from "lib/wrappers/with-color-prop"
+import { withOffsetProp } from "lib/wrappers/with-offset-prop"
+import type { Point3 } from "./translate"
 
 export type RotateProps = {
   rotation?:
     | Point3
     | [string | number, string | number, string | number]
     | string
-    | number;
+    | number
   angles?:
     | Point3
     | [string | number, string | number, string | number]
     | string
-    | number;
-  children: React.ReactNode;
-};
+    | number
+  children: React.ReactNode
+}
 
 const convertToDegrees = (value: string | number): number => {
   if (typeof value === "string") {
-    const numericValue = value.replace(/[^\d.-]/g, "");
-    const parsedValue = parseFloat(numericValue);
+    const numericValue = value.replace(/[^\d.-]/g, "")
+    const parsedValue = parseFloat(numericValue)
     if (!isNaN(parsedValue)) {
-      return parsedValue;
+      return parsedValue
     }
-    throw new Error(`Invalid rotation value: ${value}`);
+    throw new Error(`Invalid rotation value: ${value}`)
   }
-  return value;
-};
+  return value
+}
 
 const RotateBase = ({ rotation, angles, children }: RotateProps) => {
-  let finalRotation: [number, number, number];
-
   const processRotation = (
-    value: RotateProps["rotation"] | RotateProps["angles"]
+    value: RotateProps["rotation"] | RotateProps["angles"],
   ): [number, number, number] => {
     if (typeof value === "string" || typeof value === "number") {
-      const angle = convertToDegrees(value);
-      return [0, 0, angle];
+      const angle = convertToDegrees(value)
+      return [0, 0, angle]
     } else if (Array.isArray(value)) {
-      return value.map(convertToDegrees) as [number, number, number];
+      return value.map(convertToDegrees) as [number, number, number]
     } else if (value && typeof value === "object") {
       return [
         convertToDegrees(value.x),
         convertToDegrees(value.y),
         convertToDegrees(value.z),
-      ];
+      ]
     }
-    return [0, 0, 0];
-  };
+    return [0, 0, 0]
+  }
 
-  finalRotation = rotation
+  const finalRotation = rotation
     ? processRotation(rotation)
-    : processRotation(angles);
+    : processRotation(angles)
 
-  return <rotate angles={finalRotation}>{children}</rotate>;
-};
+  return <rotate angles={finalRotation}>{children}</rotate>
+}
 
-export const Rotate = withOffsetProp(withColorProp(RotateBase));
+export const Rotate = withOffsetProp(withColorProp(RotateBase))
